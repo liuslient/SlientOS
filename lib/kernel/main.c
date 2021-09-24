ed5c70ba96aab9d13cde81596a5932a7fe6d8d9b
liuslient <Liuslient@163.com>
2021-09-24 06:37:35 -0700

    打印字符函数

---
+++ lib/kernel/main.c
@@ -0,0 +1,19 @@
+#include "print.h"
+void main(void)
+{
+	
+	put_char('K');
+	put_char('e');
+	put_char('r');
+	put_char('n');
+	put_char('e');
+	put_char('l');
+	put_char('\n');
+	put_char('1');
+	put_char('2');
+	put_char('\b');
+	put_char('3');
+	while(1);
+
+}
+
