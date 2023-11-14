06ec716b8c09c94703154b9a35378882dc06b79d
liuslient <liuslient@163.com>
2023-11-14 19:18:15 +0800

    内存容量检测

---
+++ Chapter_5/div1/os.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+nasm -I include/ -o mbr.bin mbr.S
+nasm -I include/ -o loader.bin loader.S
+sudo dd if=./loader.bin of=/usr/local/hd60M.img bs=512 count=2 seek=2 conv=notrunc
+sudo dd if=./mbr.bin of=/usr/local/hd60M.img bs=512 count=1 conv=notrunc
